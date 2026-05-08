1c96b3536dc0a97e8f8771c9182afbac0d0b5cb1
Rodney Madondo <rodney@finfind.co.za>
2026-05-08 11:50:12 +0200

    added privacy and terms

---
+++ StudyReminder/Controllers/TermsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyReminder.Controllers
+{
+    public class TermsController : Controller
+    {
+      public IActionResult Terms()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
